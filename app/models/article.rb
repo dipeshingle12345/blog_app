cdc31007ad8b521432405c5a6fc75de0ecd0e703
dipeshingle12345 <mfsi.dipeshi@gmail.com>
2023-03-20 10:50:16 +0530

    callback

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article < ApplicationRecord
  validates :title, presence: true
  validates :body, presence: true, length: { minimum: 10 }
+
+ after_validation :conver_to_upcase
+
+ private
+ def conver_to_upcase
+    self.title = title.upcase if title.present?
+ end
+
+
+
 end
